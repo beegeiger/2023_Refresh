d2cacc9d8ca6a1c5e746fa22a3c58a10285d3096
beegeiger <36778471+beegeiger@users.noreply.github.com>
2023-03-28 15:38:09 -0700

    JS Review 1

--- JS.js
+++ JS.js
@@ -1,1 +1,10 @@
 JavaScript Refresh 2023
+Easy
+Third Greatest
+def ThirdGreatest(strArr):
+  strArr.remove(max(strArr, key=len))
+  strArr.remove(max(strArr, key=len))
+  return max(strArr, key=len)
+
+# keep this function call here 
+print(ThirdGreatest(input()))
