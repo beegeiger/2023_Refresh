49953c1dd1563905aeaf25354ad60d473680cc12
beegeiger <36778471+beegeiger@users.noreply.github.com>
2023-04-05 15:25:03 -0700

    2nd Try on Difficult Python Practice

--- Python.py
+++ Python.py
@@ -140,3 +140,80 @@ def PreorderTraversal(strArr):
 
 # keep this function call here 
 print(PreorderTraversal(input()))
+
+Preorder Traversal Try 2
+class Node:
+  def __init__(self, name, parent, left=None, right=None):
+    self.name = name
+    self.left = left
+    self.right = right
+    self.parent = parent
+
+  def add_right(self, right):
+    self.right = right
+
+  def add_left(self, left):
+    self.left = left
+
+
+def create_nodes(strArr):
+  line_starts = [1, 3, 7, 15]
+  line_ind_counter = 0
+  nodes = {}
+  current_line_start_ind = 0
+  input_copy = list(strArr)
+  missing_indexes = []
+  for ind, pre_nod in enumerate(strArr):
+    if pre_nod == "#":
+      if len(strArr) > (2*ind) + 2:
+        input_copy= input_copy[:(2 * ind) + 1] + ["#", "#"] + input_copy[(2 * ind) + 1:]
+  for ind, nod in enumerate(input_copy):
+    if nod != "#":
+      parent = None
+      child_type = None
+      if ind > 0:
+        node_position = ind + 1
+        if node_position % 2 == 0:
+          parent_position = (node_position / 2) - 1
+          child_type = "Left"
+        else:
+          parent_position = ((node_position -1) / 2) - 1
+          child_type = "Right"
+        parent = strArr[int(parent_position)]
+      new_node = Node(nod, parent)
+      nodes[nod] = new_node
+      if ind > 0:
+        parent_node = nodes[parent]
+        if child_type == "Left":
+          parent_node.add_left(nod)
+        elif child_type == "Right":
+          parent_node.add_right(nod)
+        nodes[parent] = parent_node
+  return nodes
+
+def check_node(current_node_name, nodes):
+  current_node = nodes[current_node_name]
+  node_order = current_node_name + " "
+  if current_node.left == None and current_node.right == None:
+    return current_node_name + " "
+  else:
+    if current_node.left:
+      left_values = check_node(current_node.left, nodes)
+      node_order += left_values
+    if current_node.right:
+      right_values = check_node(current_node.right, nodes)
+      node_order += right_values
+    return node_order
+
+def PreorderTraversal(strArr):
+  nodes = create_nodes(strArr)
+  for n in nodes:
+    nod = nodes[n]
+  first_node_name = strArr[0]
+  output = check_node(first_node_name, nodes)
+  return output[:-1]
+
+
+# keep this function call here 
+print(PreorderTraversal(input()))
+
